f169985d86897097b49e8ca2d8a693a2c380785c
TranPhat-28 <tphat8301@gmail.com>
2023-03-04 15:25:02 +0700

    Realtime message update

--- src/components/chatapp-main/MessageDisplayZone.js
+++ src/components/chatapp-main/MessageDisplayZone.js
@@ -1,82 +1,91 @@
 import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-// import { AuthContext } from '../../context/auth';
 import { SocketContext } from '../../context/socket';
 import MessageItem from './MessageItem';
 
 const MessageDisplayZone = () => {
+
   const socket = useContext(SocketContext);
 
   const { idRoom } = useParams();
   const [oldMessages, setOldMessages] = useState(null);
-  const [MessageReceive, setMessageReceive] = useState({});
-  //   const [id, setMessageReceive] = useState({});
+  const [MessageReceive, setMessageReceive] = useState([]);
+  const [receiveLength, setLength] = useState(0);
+
 
   useEffect(() => {
-    // const socket = ConnectSocket();
     async function fecthAPI() {
       const res = await axios.get(
         `http://localhost:8000/api/room/message/${idRoom}`,
         {
           headers: {
-            authorization: `Bearer ${
-              JSON.parse(localStorage.getItem('data')).token
-            }`,
+            authorization: `Bearer ${JSON.parse(localStorage.getItem('data')).token
+              }`,
           },
         }
       );
-      //   console.log(res.data);
       setOldMessages(res.data.data);
     }
+
+
     fecthAPI();
     socket.emit('join-room', idRoom);
+
     socket.on('join-room', (data) => {
-      console.log(data);
+      //console.log(data);
     });
+
     socket.on('chat-message', (data) => {
-      console.log(data);
-      setMessageReceive(data);
-      console.log(MessageReceive);
+      const temp = MessageReceive;
+      temp.push(data);
+
+      setMessageReceive(temp);
+      setLength(MessageReceive.length);
     });
+
     socket.on('connect_error', (err) => {
       if (err.message === 'xhr poll error') return;
       console.log(`connect_error :${err.message}`);
     });
+
     socket.on('handle-error', (error) => {
       console.log(error);
     });
-    // console.log(oldMessages);
-    // console.log(res.data);
 
     return () => {
       socket.off('chat-message');
       socket.off('connect_error');
       socket.off('handle-error');
     };
-  }, []);
+  }, [receiveLength]);
 
   return (
-    <div className='bg-custom-black-3 w-full h-full flex flex-col overflow-y-scroll p-4'>
-      <div className='bg-transparent w-full'>
-        {oldMessages &&
-          oldMessages.map((item, index) => {
-            console.log(item);
-            const userid = JSON.parse(localStorage.getItem('data')).userId;
-            let From = 'false';
-            if (item.userId === userid) {
-              From = 'true';
-            }
-            return (
-              //   <div></div>
+    <div className='bg-custom-black-3 w-full h-full flex flex-col-reverse overflow-y-scroll p-4' id='scroll'>
+      <div className='bg-transparent w-full' id='scroll'>
 
-              <MessageItem from={From} key={index} content={item.content} />
-            );
-          })}
+        {oldMessages && oldMessages.map((item, index) => {
+          //console.log(item);
+          const userid = JSON.parse(localStorage.getItem('data')).userId;
+          let From = 'false';
+          if (item.userId === userid) {
+            From = 'true';
+          }
+          return (
+            <MessageItem from={From} key={index} content={item.content} />
+          );
+        })}
 
-        {MessageReceive.msg && (
-          <MessageItem from='true' content={MessageReceive.msg} />
-        )}
+        {MessageReceive.map((item, index) => {
+          const userid = JSON.parse(localStorage.getItem('data')).userId;
+          let From = 'false';
+          if (item.userId === userid) {
+            From = 'true';
+          }
+          return (
+            <MessageItem from={From} key={index} content={item.msg} />
+          );
+        })}
       </div>
     </div>
   );
--- src/context/socket.js
+++ src/context/socket.js
@@ -2,8 +2,9 @@ import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
+
 function ConnectSocket() {
-  const token = JSON.parse(localStorage.getItem('data')).token;
+  const token = JSON.parse(localStorage.getItem('data'))?.token;
   if (token) {
     return io('http://localhost:8000/chat', {
       auth: {
